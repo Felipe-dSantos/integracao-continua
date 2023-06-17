9229e8ab5a61e281950e5748812a01f5b1dcc3e6
Felipe Dos Santos <99162735+Felipe-dSantos@users.noreply.github.com>
2023-06-16 21:43:14 -0300

    Update FiltroCors.java

--- sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -16,9 +16,9 @@ public class FiltroCors {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(Arrays.asList(
+            "https://sgcmapp-production.up.railway.app",
             "http://127.0.0.1:5500",
-            "https://localhost:4200",
-            "https://sgcmapp-production.up.railway.app"
+            "https://localhost:4200"
         ));
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
